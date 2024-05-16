1269d3c6e5dc9181642d50b18fad2ef056e4f43a
Danchevskyi Vladyslav <scorpio232354@gmail.com>
2024-05-16 18:55:34 +0300

    feat: new Middleware which will be checking if an authorized user can obtain access to the route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\GuestMiddleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
@@ -8,11 +11,13 @@ use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'ability' => CheckForAnyAbility::class,
+            'guest' => GuestMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,24 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Brand\BrandController;
-use App\Http\Controllers\CarModel\CarModelController;
+use App\Http\Controllers\Image\ImageController;
+use App\Http\Controllers\Photo\PhotoController;
 use App\Http\Controllers\CarPart\CarPartController;
+use App\Http\Controllers\CarModel\CarModelController;
 
 Route::get('/user', fn (Request $request) => $request->user())
     ->middleware('auth:sanctum');
 
+Route::get('/getImage/{fileName}', ImageController::class);
+
 Route::group(['as' => 'auth.', 'controller' => AuthController::class], function () {
 
     Route::post('/register', 'register')
-        ->middleware('guest:sanctum')
+        ->middleware('guest')
         ->name('register');
 
     Route::post('/login', 'login')
-        ->middleware('guest:sanctum')
+        ->middleware('guest')
         ->name('login');
 
     Route::post('/logout', 'logout')
@@ -28,6 +32,7 @@ Route::group(['as' => 'auth.', 'controller' => AuthController::class], function 
         ->middleware(['auth:sanctum', 'ability:refreshToken'])
         ->name('refresh');
 });
+
 
 Route::apiResource('brand', BrandController::class)->only(['index', 'show']);
 Route::apiResource('model', CarModelController::class)->only(['index', 'show']);
